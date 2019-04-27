223597bc38a9ba92dadb27da89e6a60832cb2509
Rishabh <rkv225@gmail.com>
2019-04-27 20:46:49 +0530

    alarm tone set

--- final/alarm_player.py
+++ final/alarm_player.py
@@ -2,6 +2,7 @@ import datetime
 import json
 import time
 from firebase import firebase
+import os
 
 class alarm_item:
     def __init__(self, id, h, m,ap):
@@ -22,16 +23,17 @@ while(True):
     current_hour = int(current_time.strftime("%I"))
     current_minute = int(current_time.strftime("%M"))
     current_ap = 'a' if current_time.strftime("%p") == 'AM' else 'p'
-
-    for key,val in fetched_alarms.items():
-        print(key,val)
-        obj=alarm_item(key,int(val['hours']),int(val['minutes']),val['ap'])
-        if(obj.ap == current_ap):
-            if(obj.hours == current_hour):
-                if(obj.minutes == current_minute):
-                    #delete the particular alarm entry from the firebase
-                    fb.delete('/alarms', obj.id)
-                    print("alarm ringing" + obj.id)
+    if(fetched_alarms):
+        for key,val in fetched_alarms.items():
+            print(key,val)
+            obj=alarm_item(key,int(val['hours']),int(val['minutes']),val['ap'])
+            if(obj.ap == current_ap):
+                if(obj.hours == current_hour):
+                    if(obj.minutes == current_minute):
+                        #delete the particular alarm entry from the firebase
+                        fb.delete('/alarms', obj.id)
+                        print("alarm ringing" + obj.id)
+                        os.system("mpg321 -g 100 alarm_tone.mp3")
 
     print(".")
     time.sleep(30)
